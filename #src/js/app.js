d7f6749d306d6709fe009ab9eb03d9177c93f950
AlexShkn <barsuk-b@yandex.ru>
2021-10-12 13:49:27 +0300

    сверстан header +burger +adapvtive

---
+++ #src/js/app.js
@@ -0,0 +1,12 @@
+//============MAIN==================
+@@include('files/regular.js', {})
+@@include('files/functions.js', {})
+@@include('files/script.js', {})
+@@include('files/forms.js', {})
+@@include('files/scroll.js', {})
+
+//===========Options================
+@@include('files/burger.js', {})
+@@include('files/dynamic_adapt.js', {})
+@@include('files/sliders.js', {})
+
